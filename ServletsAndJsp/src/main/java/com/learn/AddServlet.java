96ca415859fff2ad3450855bfd44175e63102b44
sakalesh <tanguturisakalesh@gmail.com>
2024-10-18 20:16:13 +0530

    Committed web.xml and servlet class file

---
+++ ServletsAndJsp/src/main/java/com/learn/AddServlet.java
@@ -0,0 +1,33 @@
+package com.learn;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int k = i + j;
+		PrintWriter out = res.getWriter();
+		out.println("result is " + k);
+	}
+
+//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//
+//		int k = i + j;
+//		PrintWriter out = res.getWriter();
+//		out.println("wynik: " + k);
+//	}
+
+//	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		doGet(req, res);
+//	}
+
+}
