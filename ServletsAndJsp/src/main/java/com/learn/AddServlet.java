c3cadf297029e7b5b2b569106b958571246775d6
sakalesh <tanguturisakalesh@gmail.com>
2024-10-20 20:15:11 +0530

    Using WebServlet Annotation committed

--- ServletsAndJsp/src/main/java/com/learn/AddServlet.java
+++ ServletsAndJsp/src/main/java/com/learn/AddServlet.java
@@ -5,12 +5,14 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/add") // If you use this you do not need web.xml
 public class AddServlet extends HttpServlet {
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
--- ServletsAndJsp/src/main/java/com/learn/SqServlet.java
+++ ServletsAndJsp/src/main/java/com/learn/SqServlet.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/sq") // If you use this you do not need web.xml
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		// Using RequestDispatcher
